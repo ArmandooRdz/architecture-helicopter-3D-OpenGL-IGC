9e16b1d2190eaa8a35125a8e30e82d4e74128cea
com.armandordz <1730269@upv.edu.mx>
2019-10-28 23:01:08 -0600

    cola-objeto2-agregado-al-cuerpo-objeto1

--- src/helicopter.cpp
+++ src/helicopter.cpp
@@ -20,102 +20,138 @@ using namespace std;
 void display();
 void specialKeys();
 
-double rotate_y = 0;
-double rotate_x = 0;
+double rotate_x = -10;
+double rotate_y = -10;
 
 
 
 
 
 /**
- * DIbujado del entro del cuerpo del helicuptero.
+ * DIbujado del centro del cuerpo del helicuptero.
  **/
 void drawBodyCentral(){
-    
-    
     //Side front - Gray
     glBegin(GL_POLYGON);
     glColor3f(0.50, 0.50, 0.50);
-    glVertex3f(-0.10, -0.25, -0.25); 
-    glVertex3f(0.0, 0.0, -0.25); 
-    glVertex3f(0.0, 0.25, -0.25); 
+    glVertex3f(-0.10, -0.20, -0.23); 
+    glVertex3f(0.0, 0.0, -0.23); 
+    glVertex3f(0.0, 0.25, -0.23); 
     glVertex3f(-0.50, 0.25, -0.25); 
     glVertex3f(-0.65, 0.0, -0.25);
-    glVertex3f(-0.50, -0.25, -0.25); 
+    glVertex3f(-0.50, -0.20, -0.25); 
     glEnd();
 
     //Side back - White
     glBegin(GL_POLYGON);
     glColor3f(1.0, 1.0, 1.0);
-    glVertex3f(-0.10, -0.25, 0.10);
-    glVertex3f(0.0, 0.0, 0.10);
-    glVertex3f(0.0, 0.25, 0.10);
-    glVertex3f(-0.50, 0.25, 0.10);
-    glVertex3f(-0.65, 0.0, 0.10);
-    glVertex3f(-0.50, -0.25, 0.10);
+    glVertex3f(-0.10, -0.20, 0.03);
+    glVertex3f(0.0, 0.0, 0.03);
+    glVertex3f(0.0, 0.25, 0.03);
+    glVertex3f(-0.50, 0.25, 0.05);
+    glVertex3f(-0.65, 0.0, 0.05);
+    glVertex3f(-0.50, -0.20, 0.05);
     glEnd();
 
-    //Side rigth - Purple 
-    // --------------------------------------
+    //Side rigth - Purple
     glBegin(GL_POLYGON);
     glColor3f(1.0, 0.0, 1.0); 
-    glVertex3f(0.0, 0.0, -0.25);
-    glVertex3f(0.0, 0.25, -0.25);
-    glVertex3f(0.0, 0.25, 0.10);
-    glVertex3f(0.0, 0.0, 0.10);
-    glVertex3f(-0.10, -0.25, 0.10);
-    glVertex3f(-0.10, -0.25, -0.25);
+    glVertex3f(0.0, 0.0, -0.23);
+    glVertex3f(0.0, 0.25, -0.23);
+    glVertex3f(0.0, 0.25, 0.03);
+    glVertex3f(0.0, 0.0, 0.03);
+    glVertex3f(-0.10, -0.20, 0.03);
+    glVertex3f(-0.10, -0.20, -0.23);
     glEnd();
 
     //Side left - Green
     glBegin(GL_POLYGON);
     glColor3f(0.0, 1.0, 0.0);
-    // 2 nuevos vertices
-    glVertex3f(-0.65, 0.0, 0.10);
-    glVertex3f(-0.50, 0.25, 0.10);
+    glVertex3f(-0.65, 0.0, 0.05);
+    glVertex3f(-0.50, 0.25, 0.05);
     glVertex3f(-0.50, 0.25, -0.25);
-    // 2 nuevos vertices
     glVertex3f(-0.65, 0.0, -0.25);
-    glVertex3f(-0.50, -0.25, -0.25);
-    glVertex3f(-0.50, -0.25, 0.10);
+    glVertex3f(-0.50, -0.20, -0.25);
+    glVertex3f(-0.50, -0.20, 0.05);
     glEnd();
 
     //Side up - Blue
     glBegin(GL_POLYGON);
     glColor3f(0.0, 0.0, 1.0);
-    glVertex3f(0.0, 0.25, 0.10);
-    glVertex3f(0.0, 0.25, -0.25);
+    glVertex3f(0.0, 0.25, 0.03);
+    glVertex3f(0.0, 0.25, -0.23);
     glVertex3f(-0.50, 0.25, -0.25);
-    glVertex3f(-0.50, 0.25, 0.10);
+    glVertex3f(-0.50, 0.25, 0.05);
     glEnd();
 
     //Side down - Red
     glBegin(GL_POLYGON);
     glColor3f(1.0, 0.0, 0.0);
-    glVertex3f(-0.10, -0.25, -0.25);
-    glVertex3f(-0.10, -0.25, 0.10);
-    glVertex3f(-0.50, -0.25, 0.10);
-    glVertex3f(-0.50, -0.25, -0.25);
+    glVertex3f(-0.10, -0.20, -0.23);
+    glVertex3f(-0.10, -0.20, 0.03);
+    glVertex3f(-0.50, -0.20, 0.05);
+    glVertex3f(-0.50, -0.20, -0.25);
     glEnd();
 
-    
 }
+
+/**
+ * Dibujado de la parte trasera del helicopero
+ **/
+void drawTail(){
+
+    // front - red
+    glBegin(GL_POLYGON);
+    glColor3f(0.5, 0.1, 0.1);
+    glVertex3f(0.0, 0.0, -0.23);
+    glVertex3f(0.0, 0.25, -0.23);
+    glVertex3f(0.6, 0.25, -0.15);
+    glVertex3f(0.6, 0.1, -0.15);
+    glEnd();
+
+    // back - green
+    glBegin(GL_POLYGON);
+    glColor3f(0.1, 0.7, 0.3);
+    glVertex3f(0.0, 0.0, 0.03);
+    glVertex3f(0.0, 0.25, 0.03);
+    glVertex3f(0.6, 0.25, -0.05);
+    glVertex3f(0.6, 0.1, -0.05);
+    glEnd();
+
+    //down - purple
+    glBegin(GL_POLYGON);
+    glColor3f(0.4, 0.1, 0.6);
+    glVertex3f(0.0, 0.0, -0.23);
+    glVertex3f(0.0, 0.0, 0.03);
+    glVertex3f(0.6, 0.1, -0.05);
+    glVertex3f(0.6, 0.1, -0.15);
+    glEnd();
+
+    //up - yellow
+    glBegin(GL_POLYGON);
+    glColor3f(0.9, 0.7, 0.1);
+    glVertex3f(0.0, 0.25, -0.23);
+    glVertex3f(0.0, 0.25, 0.03);
+    glVertex3f(0.6, 0.25, -0.05);
+    glVertex3f(0.6, 0.25, -0.15);
+    glEnd();
+
+}
+
 
 void display()
 {
-
     //  Borrar pantalla y Z-buffer
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
     // Resetear transformaciones
     glLoadIdentity();
-
     // Rotar cuando el usuario cambie “rotate_x” y “rotate_y”
     glRotatef(rotate_x, 1.0, 0.0, 0.0);
     glRotatef(rotate_y, 0.0, 1.0, 0.0);
 
     drawBodyCentral();
-    
+
+    drawTail();
 
     glFlush();
     glutSwapBuffers();
